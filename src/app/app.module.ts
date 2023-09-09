1908de9f27d1e7a8e67be5fea4eec64ec2a18999
John Konstas <jkonstas@cisco.com>
2023-09-09 12:46:54 +0300

    refactor: re-structure project for better maintenance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PokemonSearchComponent } from './pokemon-search/pokemon-search.component';
-import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { PokemonSearchComponent } from './pokemon/pokemon-search/pokemon-search.component';
+import { AutocompleteComponent } from './shared/autocomplete/autocomplete.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
-import { ScrollableListComponent } from './scrollable-list/scrollable-list.component';
+import { ScrollableListComponent } from './shared/scrollable-list/scrollable-list.component';
 
 @NgModule({
   declarations: [
