af909779615713cd65fa55c648ea2a0e63adf784
John Konstas <jkonstas@cisco.com>
2023-09-16 11:25:47 +0300

    chore: coalesce event change detections

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, {ngZoneEventCoalescing: true})
   .catch(err => console.error(err));
